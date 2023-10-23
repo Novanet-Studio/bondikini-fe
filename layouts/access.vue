a2ad299f5e4ad10508511be06d81899d5cc0b287
David Arenas <davejs136@gmail.com>
2023-10-23 18:55:27 -0400

    add: layouts

---
+++ layouts/access.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="access">
+    <div class="access__wrapper">
+      <header class="access__header">
+        <NuxtLink class="access__link" to="/">
+          <img
+            class="filter filter-drop-shadow"
+            src="/logo.svg"
+            alt="Project logo"
+          />
+        </NuxtLink>
+      </header>
+      <NuxtPage />
+      <AppNotify />
+    </div>
+    <div class="mt-20"><AppFooterCopyright /></div>
+  </div>
+</template>
+
+<style scoped>
+  .access {
+    @apply flex flex-col bg-light-500 w-full min-h-screen overflow-hidden relative;
+  }
+
+  .access__wrapper {
+    @apply w-full mt-12 flex-1 md:max-w-xl md:mx-auto;
+  }
+
+  .access__header {
+    @apply p-6 flex justify-center;
+  }
+
+  .access__link {
+    @apply cursor-pointer;
+  }
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="default">
+    <div class="default__wrapper">
+      <div class="default__container">
+        <div class="default__content">
+          <AppHeader />
+          <div
+            class="py-4 bg-color-6 px-16 text-xs text-color-7 font-bold flex justify-center md:text-sm lg:justify-initial lg:py-8"
+            v-if="$route.path.includes('product')"
+          >
+            <button
+              class="flex items-center gap-1 lg:ml-17"
+              @click="$router.go(-1)"
+            >
+              <span class="i-ph-arrow-left inline-block"></span>Volver
+            </button>
+          </div>
+          <main class="default__main">
+            <NuxtPage />
+          </main>
+          <AppFooter />
+          <AppNotify />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .default {
+    @apply flex relative;
+  }
+  .default__container {
+    @apply transition animate-ease-[cubic-bezier(0.4,0,0.2,1)];
+  }
+
+  .default__wrapper {
+    @apply flex-auto backface-hidden flex flex-col min-h-screen max-w-full;
+  }
+  .default__content {
+    @apply flex-auto transition ease;
+  }
+  .default__main {
+    @apply transition ease w-full mx-auto md:px-0;
+  }
+</style>
