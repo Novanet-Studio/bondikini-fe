c21368bb8255ddf112c7dfc29279ca5dbdec57e2
Fredy Polania <fpolania@gmail.com>
2023-10-31 13:24:24 -0400

    styles: project structure criteria

--- pages/company.vue
+++ pages/company.vue
@@ -7,32 +7,38 @@ definePageMeta({
 </script>
 
 <template>
-  <section>
-    <AppSlider :items="appConfig.home.slider" />
-    <div class="container mt-16">
-      <AppMessage variant="secondary">
-        Dive into our stunning range of swimsuits, where style meets
-        functionality for every beach lover’s delight. Explore endless options
-        today.
-      </AppMessage>
+  <AppSlider :items="appConfig.home.slider" />
+  <div class="company">
+    <AppMessage class="default__message" variant="secondary">
+      Dive into our stunning range of swimsuits, where style meets functionality
+      for every beach lover’s delight. Explore endless options today.
+    </AppMessage>
 
-      <div
-        class="w-full grid gap-4 my-8 md:grid-cols-2 md:mt-16 md:divide-x md:divide-color-2"
-      >
-        <p class="text-lg md:p-8 text-color-[#5A5A5A] md:text-2xl">
-          Welcome to Bondikini where you can bask in the warm rays of
-          confidence, chic vibes and fun in the sun! Our company is dedicated to
-          providing high-quality, fashionable swimwear that empowers women by
-          the beach or by the pool or on a yacht in the middle of the clear blue
-          waters.
-        </p>
-        <p class="text-lg md:p-8 text-color-[#5A5A5A] md:text-2xl">
-          Whether you're looking for subtle sophistication in black and white or
-          splashing fun with colors, textures and pearl appliques Bondikini has
-          what you're looking for. <br />
-          <b>Coming Soon:</b> swimwear accessories and more!
-        </p>
-      </div>
+    <div class="company__description">
+      <p class="company__paragraph">
+        Welcome to Bondikini where you can bask in the warm rays of confidence,
+        chic vibes and fun in the sun! Our company is dedicated to providing
+        high-quality, fashionable swimwear that empowers women by the beach or
+        by the pool or on a yacht in the middle of the clear blue waters.
+      </p>
+      <p class="company__paragraph">
+        Whether you're looking for subtle sophistication in black and white or
+        splashing fun with colors, textures and pearl appliques Bondikini has
+        what you're looking for. <br />
+        <b>Coming Soon:</b> swimwear accessories and more!
+      </p>
     </div>
-  </section>
+  </div>
 </template>
+
+<style scoped>
+.company {
+  @apply container mt-16;
+}
+.company__description {
+  @apply w-full grid gap-4 my-8 md:grid-cols-2 md:mt-16 md:divide-x md:divide-color-2;
+}
+.company__paragraph {
+  @apply text-lg md:p-8 text-[#5A5A5A] md:text-2xl;
+}
+</style>
--- pages/contact.vue
+++ pages/contact.vue
@@ -1,25 +1,35 @@
 <template>
-  <section class="container mt-14 md:mt-24">
-    <AppMessage variant="secondary">
+  <div class="contact">
+    <AppMessage class="default__message">
       Our collections offer a diverse selection of sizes and styles to suit the
       sophisticated yet fun woman who is always in style.
     </AppMessage>
 
-    <header class="w-full border-b-2 border-b-color-1 mt-12">
-      <h2 class="text-3xl font-bold pb-2">Get in touch</h2>
-    </header>
+    <h2 class="contact__header">Get in touch</h2>
 
-    <div
-      class="grid gap-12 grid-cols-1 md:grid-cols-2 md:gap-8 lg:grid-cols-[1fr_30%] lg:gap-24"
-    >
+    <div class="contact__touch">
       <ContactForm />
       <ContactInfo />
     </div>
 
-    <AppMessage class="my-16 lg:mt-24" variant="secondary">
+    <AppMessage class="default__message" variant="secondary">
       Whether you're looking for something elegant or playful, we have the
       swimwear for you. For a limited time only the same price for any swimsuit
       in its category!
     </AppMessage>
-  </section>
+  </div>
 </template>
+
+<style scoped>
+.contact {
+  @apply container mt-14 md:mt-24;
+}
+
+.contact__header {
+  @apply text-3xl font-bold border-b-2 border-b-color-1 pb-2 mt-12;
+}
+
+.contact__touch {
+  @apply grid gap-12 grid-cols-1 md:grid-cols-2 md:gap-8 lg:grid-cols-[1fr_30%] lg:gap-24;
+}
+</style>
