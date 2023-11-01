734aa5169685beb759fd082f3e4572b21113cdcb
David Arenas <davejs136@gmail.com>
2023-11-01 14:57:29 -0400

    refactor: header/mini-cart

--- components/app/header/MiniCart.vue
+++ components/app/header/MiniCart.vue
@@ -6,40 +6,69 @@ const isLoadingCart = ref(false);
 const total = computed(() => cartStore.total);
 const amount = computed(() => cartStore.amount);
 const cartProducts = computed(() => productStore.cartProducts);
+const cartItems = computed(() => cartStore.cartItems);
 
-const isOpen = ref(false);
-const miniCartRef = ref(null);
+function getQuantity(id: string) {
+  if (!cartItems.value.length) return 0;
 
-function toggleIsOpen() {
-  isOpen.value = !isOpen.value;
+  const item = cartItems.value.find((item) => item.id === id);
+
+  if (!item) return 0;
+
+  return item.quantity;
 }
 
-onClickOutside(miniCartRef, () => {
-  isOpen.value = false;
-});
+function handleRemoveProductFromCart(id: string) {
+  const cartItem = cartItems.value.find((item) => item.id === id);
+  cartStore.removeProductFromCart(cartItem as CartItem);
+}
 </script>
 
 <template>
   <div class="mini-cart__wrapper">
-    <button @click.prevent="toggleIsOpen">
-      <div class="header-actions__link">
-        <UIcon name="i-ph-bag" class="header-actions__icon" />
-        <span class="header-actions__indicator-wrapper">
-          <i class="header-actions__indicator">{{ total }}</i>
-        </span>
-      </div>
-    </button>
+    <UPopover>
+      <button>
+        <div class="header-actions__link">
+          <UIcon name="i-ph-bag" class="header-actions__icon" />
+          <span class="header-actions__indicator-wrapper">
+            <i class="header-actions__indicator">{{ total }}</i>
+          </span>
+        </div>
+      </button>
 
-    <!-- Menu -->
-    <transition name="slide-fade">
-      <template v-if="isOpen">
-        <div class="mini-cart" ref="miniCartRef">
+      <template #panel>
+        <div class="mini-cart">
           <div v-if="total > 0">
             <div class="mini-cart__body">
-              <app-loader v-if="isLoadingCart" />
+              <AppLoader v-if="isLoadingCart" />
               <template v-else-if="cartProducts?.length">
-                <div v-for="(product, index) in cartProducts" :key="index">
-                  <product-mini-cart :product="product!" />
+                <div
+                  class="flex gap-4"
+                  v-for="(product, index) in cartProducts"
+                  :key="index"
+                >
+                  <UAvatar
+                    :src="product?.images[0]?.url"
+                    icon="i-ph-image"
+                    size="xl"
+                    :ui="{ rounded: 'rounded-sm' }"
+                  />
+
+                  <div class="relative">
+                    <h6 class="font-bold">{{ product?.name }}</h6>
+                    <small>
+                      {{ getQuantity(product!.id) }} x
+                      {{ useGlobalStore().currency }}
+                      {{ product?.price }}
+                    </small>
+                  </div>
+                  <UButton
+                    icon="i-ph-x"
+                    variant="link"
+                    color="color-5"
+                    :padded="false"
+                    @click="handleRemoveProductFromCart(product!.id)"
+                  />
                 </div>
               </template>
             </div>
@@ -48,26 +77,25 @@ onClickOutside(miniCartRef, () => {
                 Sub total:
                 <strong class="mini-cart__amount">${{ amount }}</strong>
               </h3>
-              <figure class="mini-cart__wrapper">
-                <div class="mini-cart__left">
-                  <nuxt-link to="/shopping-cart" class="btn">
-                    Ver carrito
-                  </nuxt-link>
-                </div>
-                <div class="mini-cart__right">
-                  <nuxt-link to="/checkout" class="btn btn--outline">
-                    Pagar
-                  </nuxt-link>
-                </div>
+              <figure class="grid grid-cols-2 gap-2">
+                <UButton
+                  label="See cart"
+                  variant="outline"
+                  @click="$router.push('/shopping-cart')"
+                />
+                <UButton
+                  class="justify-center"
+                  label="Pay"
+                  color="color-3"
+                  @click="$router.push('/checkout')"
+                />
               </figure>
             </div>
           </div>
-          <div v-else class="mini-cart__empty">
-            No hay productos en el carrito
-          </div>
+          <div v-else class="mini-cart__empty">The cart is empty</div>
         </div>
       </template>
-    </transition>
+    </UPopover>
   </div>
 </template>
 
@@ -76,21 +104,19 @@ onClickOutside(miniCartRef, () => {
   @apply relative;
 }
 .mini-cart {
-  @apply absolute min-w-[16em] -left-44 right-0 z-30 mt-32 transition ease-linear rounded-xl shadow-xl 
-  md:min-w-[18em] md:-left-48 
-  lg:min-w-[20em] lg:-left-32;
+  @apply p-4;
 }
 
 .mini-cart__body {
-  @apply min-h-[10em] relative p-5 max-h-[20em] overflow-auto bg-white border border-white border-b-transparent;
+  @apply relative max-h-[20em] overflow-auto bg-white border border-white border-b-transparent;
 }
 
 .mini-cart__footer {
-  @apply p-[10px_20px_20px] border-t-0 bg-white;
+  @apply border-t-0 bg-white mt-4;
 }
 
 .mini-cart__footer-title {
-  @apply block mb-5 text-base font-semibold flex justify-between;
+  @apply mb-5 text-base font-semibold flex justify-between;
 }
 
 .mini-cart__amount {
@@ -98,18 +124,10 @@ onClickOutside(miniCartRef, () => {
 }
 
 .mini-cart__empty {
-  @apply min-h-[3.5em] relative p-3 overflow-auto bg-white border border-white border-b-transparent;
+  @apply relative;
 }
 
 .mini-cart__wrapper {
   @apply flex flex-nowrap justify-between items-center;
-}
-
-.mini-cart__left {
-  @apply max-w-[50%] pr-1 flex basis-full;
-}
-
-.mini-cart__right {
-  @apply max-w-[50%] pl-1 basis-full;
 }
 </style>
