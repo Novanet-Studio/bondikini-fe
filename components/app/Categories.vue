37d87ae930ddaa9c90fd62105ed400481e64f113
David Arenas <davejs136@gmail.com>
2023-11-20 18:02:05 -0400

    chore: remove commented code

--- components/app/Categories.vue
+++ components/app/Categories.vue
@@ -44,32 +44,26 @@ defineProps<Props>();
 
 <style scoped>
 .categories__header {
-  /* @apply flex items-center justify-between mt-8 border-b-2 border-b-color-2 mx-6 pb-4; */
   @apply flex items-center justify-between mt-8;
 }
 
 .categories__title {
-  /* @apply text-xl text-color-1 font-normal lg:text-4xl; */
   @apply text-xl ml-5 text-color-6 font-bold lg:text-3xl;
 }
 
 .categories__btn {
-  /* @apply block text-xs border border-red-500 px-2 py-1 rounded-xl text-red-500 md:px-4 md:py-2 md:rounded-full lg:transition lg:ease-linear lg:hover:bg-color-1/10; */
   @apply block text-xs border border-color-3 px-2 py-1 rounded-xl text-color-3 md:px-4 md:py-2 md:rounded-full lg:transition lg:ease-linear lg:hover:bg-color-3/10;
 }
 
 .categories__grid {
-  /* @apply grid place-items-center place-content-center gap-4 mt-4 grid-cols-2 md:max-w-3xl md:mx-auto lg:mx-0 lg:grid-cols-8 lg:max-w-full lg:mt-8; */
   @apply grid place-items-center place-content-center gap-4 mt-4 grid-cols-2 md:max-w-3xl md:mx-auto lg:mx-0 lg:grid-cols-4;
 }
 
 .categories__img {
-  /* @apply w-20 h-20 rounded-full object-cover ring-[3px] ring-offset-2 ring-offset-white md:w-32 md:h-32 lg:w-36 lg:h-36; */
   @apply w-20 h-20 rounded-full object-cover ring-[3px] ring-offset-2 ring-offset-white md:w-32 md:h-32 lg:w-36 lg:h-36;
 }
 
 .categories__name {
-  /* @apply text-xs text-gray-600 font-medium md:text-sm lg:text-base lg:block lg:mt-4; */
   @apply text-xs font-bold md:text-sm lg:text-base lg:block lg:mt-4;
 }
 </style>
