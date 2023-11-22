af5c5a02067f46cecf0ff75386e242f5888781c0
David Arenas <davejs136@gmail.com>
2023-11-21 20:04:14 -0400

    feat: show product image, description and actions

--- components/product/QuickView.vue
+++ components/product/QuickView.vue
@@ -1,12 +1,56 @@
-<template>
-  <div class="product-quick-view">
-    <thumbnail-quick-view />
-    <product-information-quick-view />
-  </div>
-</template>
+<script setup>
+import { injectKeys } from '~/config/constants';
 
-<style scoped>
-.product-quick-view {
-  @apply mb-0 flex flex-col flex-wrap justify-center items-start lg:flex-nowrap lg:flex-row;
-}
-</style>
+const isOpen = ref(true);
+const productStore = useProductStore();
+const size = ref({});
+
+const product = computed(() => productStore.product);
+provide(injectKeys.productDetail, product);
+</script>
+
+<template>
+  <UButton @click="isOpen = !isOpen">Open</UButton>
+  <USlideover v-model="isOpen" prevent-close>
+    <UCard
+      class="flex flex-col flex-1 divide-y-0 divide-none"
+      :ui="{
+        body: { base: 'flex-1', ring: '' },
+      }"
+    >
+      <template #header>
+        <div class="flex items-center justify-between">
+          <h3 class="font-bold text-lg md:text-2xl text-color-6">
+            {{ product.name }}
+          </h3>
+          <UButton
+            color="gray"
+            variant="ghost"
+            icon="i-heroicons-x-mark-20-solid"
+            class="-my-1"
+            @click="isOpen = false"
+          />
+        </div>
+      </template>
+      <div class="flex-1">
+        <NuxtLink :to="`/product/${product.id}`">
+          <NuxtImg
+            :src="product?.images[0]?.url"
+            :alt="product.name"
+            :placeholder="[208, 232, 10]"
+            sizes="sm:0.625em md:13.125em"
+            fit="outside"
+            class="h-60 w-full object-cover"
+          />
+        </NuxtLink>
+
+        <!-- <ProductInformationDefault /> -->
+        <ProductInformation :show-title="false" />
+        <ProductSizes v-model="size" />
+      </div>
+      <template #footer>
+        <UButton block color="color-3" size="lg">Continue</UButton>
+      </template>
+    </UCard>
+  </USlideover>
+</template>
