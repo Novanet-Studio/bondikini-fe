084bb971321ca73f73dfdb68c45af841ff4634ca
David Arenas <davejs136@gmail.com>
2023-11-22 02:50:30 -0400

    fix(shopping-cart): increase and decrease quantity

--- pages/shopping-cart.vue
+++ pages/shopping-cart.vue
@@ -13,6 +13,28 @@ const cart = useCartStore();
 const product = useProductStore();
 // const discount = ref('');
 
+const handleIncreaseQuantity = (id: string) => {
+  cart.cartItems = cart.cartItems.map((item) =>
+    item.id === id
+      ? {
+          ...item,
+          quantity: item.quantity + 1,
+        }
+      : item
+  );
+};
+
+const handleDescreaseQuantity = (id: string) => {
+  cart.cartItems = cart.cartItems.map((item) =>
+    item.id === id
+      ? {
+          ...item,
+          quantity: item.quantity > 1 ? item.quantity - 1 : item.quantity,
+        }
+      : item
+  );
+};
+
 const loadCartProducts = async () => {
   let temp: any[] = [];
   const itemsId = cart.cartItems.map((item) => item.id);
@@ -161,12 +183,8 @@ onMounted(() => {
       <template #amount-data="{ row }">
         <CustomQuantity
           v-model="row.amount"
-          @increase="cart.increaseCartItemQuantity(row.id)"
-          @descrease="cart.decreaseCartItemQuantity(row.id)"
-          :disabled="
-            row.size_stock?.find((stock) => stock.id === row.sizeData.id)
-              .inventario <= row.amount
-          "
+          @increase="handleIncreaseQuantity(row.id)"
+          @descrease="handleDescreaseQuantity(row.id)"
         />
       </template>
       <template #total-data="{ row }">
